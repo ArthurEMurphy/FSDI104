5ccf238918a8ff6c29740102ed32430cc19a4811
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-04-23 09:33:48 -0400

    Saturday
    Fixes

---
+++ Pet_Saloon_Ch_28/scripts/display.js
@@ -0,0 +1,58 @@
+// display the pets in list items
+// function displayPet(){
+    // creating the text
+    // let petsDiv = document.getElementById("pets");  // this is a div on register.html
+    // let tmp=""; // clear the field  
+    // for(let i=0;i<petSalon.pets.length;i++){
+        // let pet = petSalon.pets[i]; // get each pet
+        // let li = document.createElement("li"); // creating the li tag
+        // let text = document.createTextNode(pet.name);
+        // li.appendChild(text);
+        // petsDiv.appendChild(li);
+    // }
+    // document.getElementById("pets").innerHTML=tmp;
+// }
+
+
+
+
+// // creating the text
+//     let petsDiv = document.getElementById("pets");  // this is a div on register.html
+
+// display the pets in cards
+function displayCards(){
+    let tmp="";   
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+        tmp+=`
+        <div class="pets">
+            <h4>${pet.name}</h4>;
+            <p>${pet.age}</p>;
+            <p>${pet.gender}</p>; 
+            <p>${pet.breed}</p>;
+            <p>${pet.phone}</p>;
+        </div> 
+        `;
+    }
+    document.getElementById("pets").innerHTML=tmp;
+}
+
+
+// display in table
+function displayTable(){
+    let tmp="";   
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+        tmp+=`
+        <table class="petTable" id="petTable">
+            <td><h4>${pet.name}</h4></td>;
+            <td>${pet.age}</td>;
+            <td>${pet.gender}</td>; 
+            <td>${pet.breed}</td>;
+            <td>${pet.phone}</td>;
+        </table> 
+        `;
+    }
+    document.getElementById("pets").innerHTML=tmp;
+}
+
--- Pet_Saloon_Ch_28/scripts/register.js
+++ Pet_Saloon_Ch_28/scripts/register.js
@@ -9,24 +9,68 @@ let petSalon = {
         open:"9:00 a.m.",
         close:"8:00 p.m.",
     },
-    pets:[
-        {
-//object 1
-        },
-        {
-//object 2
-        },
-        {
-//object 3
-        }
-    ]
+    pets:[]
+}
+//name,age,gender,breed,service,owner name,contact phone
+function Pet(name,age,gender,breed,service,ownerName,contactPhone){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
+    this.owner=ownerName;
+    this.phone=contactPhone;
+}
+// get the info from the inputs and store the information
+let inputName=document.getElementById("txtName");
+let inputAge=document.getElementById("txtAge")
+let inputGender=document.getElementById("txtGender");
+let inputBreed=document.getElementById("txtBreed");
+let inputService=document.getElementById("selService");
+let inputOwner=document.getElementById("txtOwner");
+let inputPhone=document.getElementById("txtTel");
+
+function isValid(aPet){
+    //return false when the pet is not valid
+    // return true if the pet is valid
+    let valid=true;
+    if(aPet.petName.length==0){
+        // if we get here it means that the name is not valid
+        valid=false;
+        console.error("Invalid name");
+    }
+    if(aPet.service.length==0){
+        valid=false;
+        console.error("Invalid service");
+    }
+    if(aPet.phone.length==0){
+            valid=false;
+            console.error("Invalid phone");
+    }
+    return valid; // it could be true or false
 }
 
-console.log(petSalon);
-function displaySalonInfo(){
-
+function register(){
+    // create the pet
+    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,input.Breed.value,inputService.value,inputOwner.value,inputPhone.value);
+    console.log(thePet);
+    if(isValid(thePet)){
+        // push the pet into the array
+        petSalon.pets.push(thePet);
+        displayPet();
+        // Clear the inputs
+        clearInputs();
+    }
 }
 
-function displayPetNames(){
-
+function clearInputs(){
+    inputName.value="";
+    inputAge.value="";
 }
+// Create pets
+let scooby = new Pet("Scooby",50,"Male","Dane","Grooming","Shaggy","555-555-5555");
+let scrappy = new Pet("Scrappy",40,"Male","Mixed","Nails cut","Shaggy","555-555-5554");
+let cheddar = new Pet("Cheddar",30,"Female","Corgi","Grooming","Short","555-555-5553");
+let nacho = new Pet("Nacho",20,"Male","Mixed","Teeth brush","Curly","555-555-5552");
+petSalon.pets.push(scooby,scrappy,cheddar,nacho);
+displayPet()
